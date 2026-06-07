daf6f544127a212cc0e99b5639e9426ede643cc0
zankoAn <zankoan4@gmail.com>
2026-06-08 00:16:23 +0330

    fix: Remove extra variable

--- utils/load_env.py
+++ utils/load_env.py
@@ -4,10 +4,10 @@ env = environ.Env()
 
 environ.Env.read_env(env_file=".env")
 
+
 class CONFIG:
     SECRET_KEY = env("SECRET_KEY", default="")  # type: ignore
     DEBUG = env.bool("DEBUG", default=False)  # type: ignore
     ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="").split(",")  # type: ignore
     CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS", default="").split(",")  # type: ignore
     CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", default="").split(",")  # type: ignore
-    DEFAULT_RECIVER_ID = env("DEFAULT_RECIVER_ID")
