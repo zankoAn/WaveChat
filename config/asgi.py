83936d64d9d3c8bbbd228c72b54572b652180e78
zankoAn <zankoan4@gmail.com>
2026-06-08 17:31:36 +0330

    clean up

--- config/asgi.py
+++ config/asgi.py
@@ -5,11 +5,12 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-from apps.chat.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from django.core.asgi import get_asgi_application
+
+from apps.chat.routing import websocket_urlpatterns
 
 django_asgi = get_asgi_application()
 static_asgi = ASGIStaticFilesHandler(django_asgi)
